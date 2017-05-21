2314a1e2444459d7a0c9385f53c1737f96d2afad
Daniel Braun <danielbraun@me.com>
2017-05-21 12:17:33 +0300

    Initial

---
+++ src/clj_http/middleware/request_cache.clj
@@ -0,0 +1,28 @@
+(ns clj-http.middleware.request-cache
+  (:require [clj-http.client :as http]
+            [clj-http.headers :as headers]
+            [clojure.core.cache :as cache]
+            [ring.util.codec :refer [url-encode]])
+  (:import (org.apache.commons.io IOUtils)))
+
+(defn- cache-key [{:keys [server-name uri query-string]}]
+  (str server-name uri "?" (url-encode (or query-string ""))))
+
+(defn wrap-request-cache [client cache-instance]
+  (fn [request]
+    (let [k (cache-key request)]
+      (if (cache/has? @cache-instance k)
+        (do
+          (swap! cache-instance cache/hit k)
+          (update (cache/lookup @cache-instance k)
+                  :headers
+                  (partial into (headers/header-map))))
+        (let [response (client request)]
+          (when (and (http/success? response)
+                     (#{:get} (:request-method request)))
+            (swap! cache-instance
+                   cache/miss
+                   k
+                   (update (client request)
+                           :body #(IOUtils/toByteArray %))))
+          response)))))
