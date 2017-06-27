37f87577611c437ed5b8ba3eff281b013a5d9b42
Daniel Braun <danielbraun@me.com>
2017-06-27 13:07:07 +0300

    Added middleware options
    Cache key can now be custom and debug messages can be turned off.

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-codec "1.0.1"]
                  [org.clojure/core.cache "0.6.5"]])
--- src/clj_http/middleware/request_cache.clj
+++ src/clj_http/middleware/request_cache.clj
@@ -4,19 +4,22 @@
             [clojure.core.cache :as cache])
   (:import (org.apache.commons.io IOUtils)))
 
-(defn- cache-key [{:keys [server-name uri query-string]}]
+(defn- default-cache-key [{:keys [server-name uri query-string]}]
   (str server-name uri (.hashCode (str "?" query-string))))
 
-(defn wrap-request-cache [client cache-instance]
+(defn wrap-request-cache
+  [client cache-instance & {:keys [cache-key debug?]
+                            :or {cache-key default-cache-key
+                                 debug? true}}]
   (fn [request]
     (let [k (cache-key request)]
       (if (cache/has? @cache-instance k)
-        (do (println "Cache hit:" k)
+        (do (when debug? (println "Cache hit:" k))
             (swap! cache-instance cache/hit k)
             (update (cache/lookup @cache-instance k)
                     :headers
                     (partial into (headers/header-map))))
-        (do (println "Cache miss:" k)
+        (do (when debug? (println "Cache miss:" k))
             (let [response (client request)]
               (when (and (http/success? response)
                          (#{:get} (:request-method request)))
